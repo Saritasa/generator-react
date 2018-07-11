d4c30d1fcde2a27f0b221788158a4a578fadc791
Konstantin Petryaev <numminorihsf@gmail.com>
2018-07-11 11:24:29 +0700

    Make feature generator to to work via BaseSubGenerator
    This allowed to simplify its code and have consistent behavior
    for every generator.

--- generators/feature/index.js
+++ generators/feature/index.js
@@ -1,101 +1,49 @@
-const path = require('path');
-const cp = require('child_process');
+const BaseSubGenerator = require('../_base/BaseSubGenerator');
 
-const Generator = require('yeoman-generator');
+const DESTINATION_FOLDER = 'features';
 
-const DEST = 'features';
-const SOURCE_PATH = 'src';
+const TEMPLATES = {
+  files: ['components/ExampleHeader.js', 'links/index.js', 'pages/ExampleRootPage.js', 'pages/ExampleNestedPage.js', 'routes/index.js', 'index.js'],
+};
+const NAMED_TEMPLATES = {
+  files: ['documentation.yml', 'links/root.link.js', 'links/nested.link.js', 'routes/paths.js', 'routes/routeStore.js', 'inject.js'],
+};
 
-module.exports = class extends Generator {
-
+module.exports = class FeatureGenerator extends BaseSubGenerator {
   constructor(args, opts) {
     // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
 
-    this.argument('feature-name', {
-      type: String,
-      description: 'Feature\'s name.',
-      required: true
-    });
+    this.mainArgument(
+      'feature',
+      "Feature's name. Use CamelCase for it. You may use `mainFeatureName/FeatureName` to create sub-feature.",
+    );
+  }
 
-    this.option('flow', { type: Boolean, default: true, description: 'Use --no-flow to remove flow from generated code' });
-    this.option('unit', { type: Boolean, default: true, description: 'Use --no-unit to prevent tests\' generating' });
+  // used to use lower-cased feature names in paths
+  _calcDest() {
+    this.options.dest = [
+      ...this.options.featureName.split('/').filter(Boolean).map(part => `features/${part}`),
+      this._dest,
+      this.options.name,
+    ].join('/');
+  }
 
-    this.option('source-root', { type: String, default: SOURCE_PATH, description: 'Path for source\'s root' });
+  // used to use prevent extra "feature" inside module
+  _calcModuleName() {
+    this.options.moduleName = [
+      this.options.FeatureName,
+      this.options.Name,
+    ].filter(Boolean).join('/');
   }
 
   initializing() {
-    this._checkNodejsVerion();
-    this._calcEntityNames();
-    this._calcModuleName();
-    this._checkArguments();
-    this.destinationRoot(this.destinationPath(this.options['source-root']));
-  }
-
-  _checkNodejsVerion() {
-    const { node } = process.versions;
-    if (Number(node.split('.')[0]) !== 8) {
-      throw new Error(`Use nodejs v8 instead of ${node}`);
-    }
-  }
-  _calcEntityNames() {
-    const [name, ...prefixParts] = this.options['feature-name'].split('/').reverse();
-
-    const featureName = prefixParts.reverse().join('/');
-
-    this.options.name = `${name[0].toLowerCase()}${name.slice(1)}`;
-    this.options.Name = `${name[0].toUpperCase()}${name.slice(1)}`;
-    this.options.featureName = `${featureName.slice(0, 1).toLowerCase()}${featureName.slice(1)}`;
-    this.options.FeatureName = `${featureName.slice(0, 1).toUpperCase()}${featureName.slice(1)}`;
-    this.options.dest = [this.options.featureName, DEST].filter(Boolean).join('/');
-  }
-
-  _calcModuleName() {
-    this.options.moduleName = [this.options.FeatureName, this.options.Name].filter(Boolean).join('/');
-  }
-
-  _checkArguments() {
-    if (!(/^[A-Z][a-zA-Z]*$/.test(this.options.Name))) {
-      throw new Error(`name should includes only latin letters and be CamelCased. You passed "${this.options.Name}"`);
-    }
-  }
-
-  _writeUnnamed() {
-    const { name, Name, flow, stories, unit, dest } = this.options;
-    const files = ['components/ExampleHeader.js', 'links/index.js', 'pages/ExampleRootPage.js', 'pages/ExampleNestedPage.js', 'routes/index.js', 'index.js'];
-
-    if (stories) files.push(...[]);
-    if (unit) files.push(...[]);
-
-    files.forEach(file => {
-      this.fs.copyTpl(
-        this.templatePath(`${file}.ejs`),
-        this.destinationPath(path.join(dest, name, `${file}`)),
-        { name, Name, flow },
-      );
-    });
-  }
-
-  _writeNamed() {
-    const { name, Name,flow, stories, unit, dest } = this.options;
-    const files = ['links/root.link.js', 'links/nested.link.js', 'routes/paths.js', 'routes/routeStore.js', 'inject.js'];
-
-    if (stories) files.push(...[]);
-    if (unit) files.push(...[]);
-
-    files.forEach(file => {
-      const [fileName, ...paths] = file.split('/').reverse();
-
-      this.fs.copyTpl(
-        this.templatePath(`${file}.ejs`),
-        this.destinationPath(path.join(dest, name, ...paths.reverse(), `${Name}.${fileName}`)),
-        { name, Name, flow },
-      );
-    });
+    this.setDestination(DESTINATION_FOLDER);
+    super.initializing();
   }
 
   writing() {
-    this._writeUnnamed();
-    this._writeNamed();
+    this.writeTemplates(TEMPLATES);
+    this.writeNamedTemplates(NAMED_TEMPLATES);
   }
 };
